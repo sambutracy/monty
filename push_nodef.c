a0579f062ce7c2207aaa4b04baec153bd193fc3e
vintage254 <derricknjuguna414@gmail.com>
2024-01-16 06:37:15 -0800

    9,10

---
+++ push_nodef.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+/**
+ * push_node - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void push_node(stack_t **head, unsigned int count)
+{
+	int n, j = 0, control = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				control = 1; }
+		if (control == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", count);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
+}
+
---
+++ top_stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * print_topstack - prints the top
+ * @head: stack head
+ * @count: line_number
+ * Return: no return
+*/
+void print_topstack(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 0;
+}
