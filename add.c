2ff93d4e7849ca8b7a6111372c5d0aef318aa920
vintage254 <derricknjuguna414@gmail.com>
2024-01-16 06:26:09 -0800

    5,6

---
+++ add.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * add_funct - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void add_funct(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	int length = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
---
+++ rotate_stack.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+  *rotate - rotates the stack to the bottom
+  *@head: stack head
+  *@count: line_number
+  *Return: no return
+ */
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int count)
+{
+	stack_t *new;
+
+	new = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (new->next)
+	{
+		new = new->next;
+	}
+	new->next = *head;
+	new->prev->next = NULL;
+	new->prev = NULL;
+	(*head)->prev =new;
+	(*head) = new;
+}
+
