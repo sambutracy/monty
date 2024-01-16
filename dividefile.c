9c21f1699cdcfd77aa8a2184daf753f3b21f6289
vintage254 <derricknjuguna414@gmail.com>
2024-01-16 06:13:07 -0800

    1,2

---
+++ dividefile.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+/**
+ * divide - divides the top two elements of the stack.
+ * @head: stack head
+ * @count: line_number
+ * Return: no return
+*/
+void divide(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	int length = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
---
+++ mod_file.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+/**
+ * mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
+ * @head: stack head
+ * @count: line_number
+ * Return: no return
+*/
+void mod(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	int length = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (lenght < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
